b798e397813db1cb2490b3ed222341161a7e7b61
nimetsuccumbs <sofia_chaves@estudante.sesisenai.org.br>
2024-02-26 09:50:20 -0300

    versao legal

---
+++ usenavigate/src/pages/home.jsx
@@ -0,0 +1,26 @@
+// Importa o módulo React do pacote react
+import React from 'react';
+// Importa o hook useNavigate do pacote react-router-dom
+import { useNavigate } from 'react-router-dom';
+// Define a função do componente Home
+function home() {
+  // Obtém a função de navegação usando o hook useNavigate
+  const irPara= useNavigate();
+  // Define a função handleClick que será chamada ao clicar no botão
+  const handleClick = () => {
+    // Navegar para a página Sobre ao clicar em algum botão
+    irPara('/sobre');
+  };
+  // Retorna a estrutura JSX da página inicial
+  return (
+    <div className='container'>
+      {/* Título da página inicial */}
+      <h1><mark>Página Inicial</mark></h1>      
+      {/* Botão para navegar para a página About ao ser clicado */}
+      <button onClick={handleClick}><b>Ir para Sobre</b></button>
+      <br/><br/>
+    </div>
+  );
+}
+// Exporta o componente Home como padrão
+export default home;
---
+++ usenavigate/src/pages/sobre.jsx
@@ -0,0 +1,26 @@
+// Importa o módulo React do pacote react
+import React from 'react';
+// Importa o hook useNavigate do pacote react-router-dom
+import { useNavigate } from 'react-router-dom';
+// Define a função do componente Sobre
+function Sobre() {
+  // Obtém a função de navegação usando o hook useNavigate
+  const irPara= useNavigate();
+  // Define a função handleClick que será chamada ao clicar no botão
+  const handleClick = () => {
+    // Navegar para a página Home ao clicar em algum botão
+    irPara('/');
+  };
+  // Retorna a estrutura JSX da página inicial
+  return (
+    <div className='container'>
+      {/* Título da página inicial */}
+      <h1><mark>Página Sobre</mark></h1>      
+      {/* Botão para navegar para a página About ao ser clicado */}
+      <button onClick={handleClick}><b>Ir para Home</b></button>
+      <br/><br/>
+    </div>
+  );
+}
+// Exporta o componente Home como padrão
+export default Sobre;
